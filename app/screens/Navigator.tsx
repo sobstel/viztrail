30c0110be213363fc236438273054892dc683e8b
Przemek Sobstel <przemek@sobstel.dev>
2022-02-03 22:06:04 +0100

    set up navigation

--- app/screens/Navigator.tsx
+++ app/screens/Navigator.tsx
@@ -1,47 +1,95 @@
 import React from 'react';
-// import { useTheme } from 'native-base';
-import { NavigationContainer } from '@react-navigation/native';
+import { Button, Center, Text } from 'native-base';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { HeaderBackButton } from '@react-navigation/elements';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 // import HomeScreen from './HomeScreen';
 import TrailScreen from './TrailScreen';
 
-const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
+function HomeEmptyScreen() {
+  const navigation: any = useNavigation();
 
-// Stack.Navigator  screenOptions={{
-// const { colors } = useTheme();
-// headerStyle: { backgroundColor: colors.coolGray[800] },
-// headerTintColor: colors.lightText,
-// headerTitleStyle: { fontWeight: 'bold', },
-// headerShadowVisible: false,
-//
-//
-
-function TrailTabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen
-        name="Trail"
-        component={TrailScreen}
-        options={{ headerShown: false }}
-      />
-    </Tab.Navigator>
+    <Center my="4">
+      <Text mb="4">under construction</Text>
+      <Button onPress={() => navigation.navigate('Trail')}>Trail</Button>
+    </Center>
   );
 }
+
+function TrailEmptyScreen() {
+  return (
+    <Center>
+      <Text>under construction</Text>
+    </Center>
+  );
+}
+
+const HomeTab = createBottomTabNavigator();
+
+function HomeTabs() {
+  return (
+    <HomeTab.Navigator screenOptions={{ headerShown: false }}>
+      <HomeTab.Screen name="AllTrails" component={HomeEmptyScreen} />
+      <HomeTab.Screen name="FavTrails" component={HomeEmptyScreen} />
+      <HomeTab.Screen name="MyTrails" component={HomeEmptyScreen} />
+    </HomeTab.Navigator>
+  );
+}
+
+const TrailTab = createBottomTabNavigator();
+
+function TrailTabs() {
+  const navigation: any = useNavigation();
+
+  return (
+    <TrailTab.Navigator screenOptions={{ headerShown: false }}>
+      <TrailTab.Screen
+        name="TrailMap"
+        component={TrailScreen}
+        options={{
+          title: 'Map',
+          headerLeft: () => (
+            <HeaderBackButton onPress={() => navigation.goBack(null)} />
+          ),
+        }}
+      />
+      <TrailTab.Screen
+        name="TrailPeople"
+        component={TrailEmptyScreen}
+        options={{
+          title: 'People',
+        }}
+      />
+      <TrailTab.Screen
+        name="TrailMe"
+        component={TrailEmptyScreen}
+        options={{
+          title: 'Me',
+        }}
+      />
+    </TrailTab.Navigator>
+  );
+}
+
+//
+
+const MainStack = createNativeStackNavigator();
 
 export default function Navigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        {/* <Stack.Screen */}
-        {/*  name="Home" */}
-        {/*  component={HomeScreen} */}
-        {/*  options={{ title: 'Trails' }} */}
-        {/* /> */}
-
-        <Stack.Screen name="Trail" component={TrailTabs} />
-      </Stack.Navigator>
+      <MainStack.Navigator>
+        <MainStack.Screen
+          name="Home"
+          component={HomeTabs}
+          options={{ title: 'Trails' }}
+        />
+        <MainStack.Screen name="Trail" component={TrailTabs} />
+        {/* TODO: UserTrail screen */}
+        {/* TODO: Settings screen */}
+      </MainStack.Navigator>
     </NavigationContainer>
   );
 }
