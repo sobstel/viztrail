09f5e02d308d7fa041f6c77b7d0529a8add4a00c
Przemek Sobstel <przemek@sobstel.dev>
2022-05-06 17:14:46 +0200

    re-enable tsc as a pre-build step

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,34 @@
 import './style.css'
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const app = document.querySelector<HTMLDivElement>('#app')!;
 
 // TODO routing
 
-app.innerHTML = `
-  <h1>Viztrail</h1>
-  <div>
-    <label for="file">Choose file to upload</label>
-    <input
-      id="upload-input"
-      type="file"
-      accept=".gpx"
-    />
-  </div> 
-`
+// HOME PAGE
+(function() {
+    app.innerHTML = `
+      <h1>Viztrail</h1>
+      <div>
+        <label for="file">Choose file to upload</label>
+        <input
+          id="upload-input"
+          type="file"
+          accept=".gpx"
+        />
+      </div>`
 
-document.querySelector<HTMLInputElement>('#upload-input')!.addEventListener('change', async (event) => {
-  const file = event.target.files[0];
-  // TODO: progress bar and uploader
-  const content = await file.text();
-  console.log(content);
+    document.querySelector<HTMLInputElement>('#upload-input')!.addEventListener('change', async (event) => {
+      const file = (event.target as HTMLInputElement).files?.[0];
+      if (!file) {
+        return;
+      }
 
-  // TODO: parse it's correct s3
-  // TODO:
-  // TODO: send to blackbaze
-});
+      // TODO: progress bar and uploader
+      const content = await file.text();
+      console.log(content);
+
+      // TODO: parse it's correct s3
+      // TODO: ...
+      // TODO: send to blackbaze
+    });
+  })();
